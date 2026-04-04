cd69b21c227a7d71db3dc8125134db63ad9a65e6
HARSH VARDHAN GOUTAM <harshgoutam0415@gmail.com>
2026-04-04 12:22:35 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+model=load_model('lstm_model.h5')
+with open('tokenizer.pkl', 'rb') as file:
+    tokenizer = pickle.load(file)
+
+reverse_index = {idx: word for word, idx in tokenizer.word_index.items()}
+
+max_len=44
+
+def generate_text(seed_text, num_words=10):
+    text=seed_text
+    for _ in range(num_words):
+        seq=tokenizer.texts_to_sequences([text])[0]
+        padded=pad_sequences([seq], maxlen=max_len, padding='pre')
+        preds=model.predict(padded, verbose=0)[0]
+        pos=np.argmax(preds)
+        next_word=reverse_index.get(pos, '')
+        text += ' ' + next_word
+    return text
+
+st.title("Next Word Prediction")
+seed_text = st.text_input("Enter a starting text:")
+num_words = st.slider("Number of words to generate:", 1, 20, 10)
+
+if st.button("Generate"):
+    result = generate_text(seed_text, num_words)
+    st.subheader("Generated Text:")
+    st.write(result)
